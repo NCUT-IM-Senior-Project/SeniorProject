94b07c3af579d47a60e046b492cb9132096d0f22
3B032104 <3B032104@gm.student.ncut.edu.tw>
2024-02-06 15:50:54 +0800

    新增路由司機driver route並設置路由權限

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+/* 需要登入才能進入的路由 */
 Route::middleware('auth')->group(function () {
     //儀表板-首頁 dashboard
     Route::get('/', function () {return view('dashboard');});
@@ -24,10 +25,24 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    /* 生管人員才能進入的路由 */
     Route::middleware('checkPermissions:1')->group(function () {
 
+        //管理司機資料
+        Route::get('driver', [DriverController::class, 'index'])->name('driver.index');
+        //管理司機資料-搜尋
+        Route::get('driver/search', [DriverController::class, 'search'])->name('driver.search');
+        //管理司機資料-新增
+        Route::post('driver', [DriverController::class, 'store'])->name('driver.store');
+        //管理司機資料-編輯
+        Route::get('driver/{driver}/edit', [DriverController::class, 'edit'])->name('driver.edit');
+        //管理司機資料-更新
+        Route::patch('driver/{editDriver}', [DriverController::class, 'update'])->name('driver.update');
+        //管理司機資料-刪除
+        Route::delete('driver/{driver}', [DriverController::class, 'destroy'])->name('driver.destroy');
     });
 
+    /* 司機才能進入的路由 */
     Route::middleware('checkPermissions:2')->group(function () {
 
     });
