cc508d8fa62cb7277717e229c14fe0c66cd06407
3B032104 <3B032104@gm.student.ncut.edu.tw>
2024-02-06 15:52:03 +0800

    新增司機資料管理功能：新增、修改、刪除、總列表

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDriverRequest;
+use App\Http\Requests\UpdateDriverRequest;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $drivers = User::paginate(9);
+        return view('driver.index', compact('drivers'));
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $drivers = User::where('name', 'like', '%' . $search . '%')->paginate(9);
+
+        return view('driver.index', compact('drivers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDriverRequest $request)
+    {
+
+        try {
+            // 獲取已驗證的數據
+            $validatedData = $request->validated();
+
+            // 在已驗證的數據中添加欄位
+            $validatedData['status'] = 0;
+            $validatedData['permission_id'] = 2;
+
+            // 驗證成功
+            User::create($validatedData);
+
+            // 資料保存後轉跳回新書總表
+            return redirect(route('driver.index'))->with([
+                'success' => '帳號新增成功！',
+                'type' => 'success',
+            ]);
+        } catch (QueryException $e) {
+            // 檢查是否是唯一性約束違規（錯誤碼 1062）
+            if ($e->errorInfo[1] == 1062) {
+                // 根據需要處理重複 email 的情況
+                return redirect()->back()->with([
+                    'success' => '帳號新增失敗，該 email 已經存在。',
+                    'type' => 'error',
+                ])->withErrors(['email' => '該 email 已經存在']);
+            }
+
+            // 其他錯誤情況的處理
+            return redirect()->back()->with([
+                'error' => '帳號新增失敗，請稍後再試。',
+                'type' => 'error',
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $driver)
+    {
+        $editDriver = $driver;
+        $drivers = User::paginate(9);
+        return view('driver.index', compact('drivers', 'editDriver'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDriverRequest $request, User $editDriver)
+    {
+        $editDriver->update($request->validated());
+
+        return redirect(route('driver.index'))->with([
+            'success' => '司機 '. $editDriver-> name . ' 資料更新成功！',
+            'type' => 'success',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $driver)
+    {
+        $driver->delete();
+
+        return redirect(route('driver.index'))->with([
+            'success' => '書籍 [編號：'. $driver-> id.'] 刪除成功！',
+            'type' => 'success',
+        ]);
+    }
+}
---
+++ resources/views/driver/edit.blade.php
@@ -0,0 +1,58 @@
+<!-- 修改司機資訊 -->
+<p class="transition duration-1000 text-gray-600 text-sm dark:text-gray-200">修改司機資訊</p>
+<div class="bg-white dark:bg-gray-800 text-gray-800 dark:text-white">
+    <div class="mx-auto max-w-full px-4 py-4 sm:px-6 sm:py-4 lg:px-8">
+        <form action="{{ route('driver.update', $editDriver) }}" method="POST" role="form">
+            @method('PATCH')
+            @csrf
+
+            <div class="grid grid-cols-4 grid-rows-2 gap-2">
+                <div class="col-span-4 sm:col-span-1">
+                    <label for="name" class="block text-sm font-medium mb-2 dark:text-white">姓名</label>
+                    <input type="text" id="name" name="name" value="{{ old('name', $editDriver -> name ) }}" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="請輸入姓名...">
+                    @error('name')
+                    <div class="text-red-400">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-span-4 sm:col-span-1">
+                    <label for="phone" class="block text-sm font-medium mb-2 dark:text-white">連絡電話</label>
+                    <input type="text" id="phone" name="phone" value="{{ old('phone', $editDriver -> phone ) }}" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="請輸入連絡電話...">
+                    @error('phone')
+                    <div class="text-red-400">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-span-4 sm:col-span-1">
+                    <label for="email" class="block text-sm font-medium mb-2 dark:text-white">帳號</label>
+                    <input type="email" id="email" name="email" value="{{ old('email', $editDriver -> email ) }}" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="請輸入帳號...">
+                    @error('email')
+                    <div class="text-red-400">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-span-4 sm:col-span-1">
+                    <label for="password" class="block text-sm font-medium mb-2 dark:text-white">密碼</label>
+                    <input type="password" id="password" name="password" value="{{ old('password', $editDriver -> password ) }}" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="請輸入密碼...">
+                    @error('password')
+                    <div class="text-red-400">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="row-start-2 col-span-4 sm:col-span-2">
+                    <label for="description" class="block text-sm font-medium mb-2 dark:text-white">備註</label>
+                    <textarea id="description" name="description"  class="py-3 px-4 block resize-x w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" rows="1" placeholder="請輸入文字...">{{ old('description', $editDriver->description) }}</textarea>
+                    @error('description')
+                    <div class="text-red-400">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="flex sm:justify-end items-end space-x-2 sm:col-start-4 sm:row-start-2 col-span-4 row-start-6 ">
+                    <a href="{{ route('driver.index') }}" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-300 text-gray-800 hover:bg-gray-400 dark:bg-slate-600 dark:text-gray-400 dark:hover:bg-slate-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                        取消
+                    </a>
+                    <button type="submit" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                        修改
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
