60130c0e88165fb41c0bf0d2c6f155294deeae21
3B032104 <3B032104@gm.student.ncut.edu.tw>
2024-03-06 23:31:35 +0800

    新增客戶資料管理功能：總列表、詳細彈窗

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
+use App\Models\ContactPerson;
+use App\Models\RequirementData;
+use App\Models\RequirementItem;
+use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
@@ -13,7 +17,43 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::paginate(9);
+
+        $clients->each(function ($client) {
+            // 取得聯絡人資訊
+            $contactPerson = ContactPerson::where('partner_id', $client->partner_id)
+                ->select('name', 'phone')
+                ->first();
+
+            // 將聯絡人資訊加入客戶資料
+            $client->contactPeopleName = $contactPerson ? $contactPerson->name : null;
+            $client->contactPeoplePhone = $contactPerson ? $contactPerson->phone : null;
+
+            // 取得需求編號資料
+            $requirementData = RequirementData::where('partner_id', $client->partner_id)
+                ->pluck('requirement_items_id');
+
+            //取得需求項目名稱陣列
+            $requirementItems = RequirementItem::whereIn('id', $requirementData)
+                ->pluck('name')
+                ->toArray();
+
+            $client->requirementItems = implode(', ', $requirementItems);
+
+        });
+
+        return view('client.index', compact('clients'));
+    }
+
+    /**
+     * Search the specified resource in storage.
+     */
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $clients = Client::where('name', 'like', '%' . $search . '%')->paginate(9);
+
+        return view('client.index', compact('clients'));
     }
 
     /**
